60864a09fca86aec5a2d90518d2ff63cf6f8726d
Stephie <stephaniecrocker91@gmail.com>
2023-09-07 15:03:10 +0000

    Add countdowner folder and components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import CarouselComponent from './components/Carousel';
 import LandingOverlay from './components/LandingOverlay'
+import CountdownTimer from './components/CountdownTimer/CountdownTimer';
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     
     <LandingOverlay />
     <CarouselComponent />
-
+    <CountdownTimer countdownTimestampMs={1700784000000}/>
     </>
   );
 }
---
+++ src/components/CountdownTimer/CountdownTimer.js
@@ -0,0 +1,45 @@
+import styles from '../../styles/CountdownTimer.module.css'
+import { useState, useEffect } from 'react';
+import {getRemainingTimeUntilMsTimestamp} from './Utils/CountdownTimerUtils';
+
+{/** Requires installing dayjs with command npm install dayjs*/}
+
+
+const defaultRemainingTime = {
+    seconds: '00',
+    minutes: '00',
+    hours: '00',
+    days:'00',
+}
+
+const CountdownTimer = ({countdownTimestampMs}) => {
+    const [remainingTime, setRemainingTime] = useState(defaultRemainingTime);
+    
+useEffect(() => {
+    const intervalId = setInterval(() => {
+        updateRemainingTime(countdownTimestampMs);
+    }, 1000);
+    return () => clearInterval(intervalId);
+},[countdownTimestampMs]);
+
+    function updateRemainingTime(countDown){
+        setRemainingTime(getRemainingTimeUntilMsTimestamp(countDown));
+    }
+
+    return(
+        <div className={styles.Timer}>
+            <div className = {styles.countdown}>
+                <span><strong>{remainingTime.days}</strong></span>
+                <span>d |</span>
+                <span className={styles.twonumbers}><strong>{remainingTime.hours}</strong></span>
+                <span>h |</span>
+                <span className={styles.twonumbers}><strong>{remainingTime.minutes}</strong></span>
+                <span>m |</span>
+                <span className={styles.twonumbers}><strong>{remainingTime.seconds}</strong></span>
+                <span>s</span>
+            </div>
+        </div>
+    );
+}
+
+export default CountdownTimer
